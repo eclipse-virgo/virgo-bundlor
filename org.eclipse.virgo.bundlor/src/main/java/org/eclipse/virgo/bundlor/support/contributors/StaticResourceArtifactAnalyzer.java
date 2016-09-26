3506b7cd74b9912f7c48f61ce2ee3611426ed08b
Daniel Marthaler <dmarthaler@gmx.net>
2016-09-26 16:38:03 +0200

    bug 445722: Fixed issue in StaticResourceArtifactAnalyzer

--- org.eclipse.virgo.bundlor/src/main/java/org/eclipse/virgo/bundlor/support/contributors/StaticResourceArtifactAnalyzer.java
+++ org.eclipse.virgo.bundlor/src/main/java/org/eclipse/virgo/bundlor/support/contributors/StaticResourceArtifactAnalyzer.java
@@ -12,6 +12,7 @@
 package org.eclipse.virgo.bundlor.support.contributors;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 import org.eclipse.virgo.bundlor.support.ArtifactAnalyzer;
@@ -33,10 +34,17 @@ public final class StaticResourceArtifactAnalyzer implements ArtifactAnalyzer {
      * {@inheritDoc}
      */
     public void analyse(InputStream artefact, String artefactName, PartialManifest partialManifest) throws Exception {
-        char osPathSeparator = File.separatorChar;
-        int index = artefactName.lastIndexOf(osPathSeparator);
+        char pathSeparator = '/';
+        if (artefact instanceof FileInputStream) {
+            pathSeparator = File.separatorChar;
+        }
+        analyseInternal(artefact, artefactName, partialManifest, pathSeparator);
+    }
+
+    private void analyseInternal(InputStream artefact, String artefactName, PartialManifest partialManifest, char pathSeparator) throws Exception {
+        int index = artefactName.lastIndexOf(pathSeparator);
         if (index > -1) {
-            String packageName = artefactName.substring(0, index).replace(osPathSeparator, '.');
+            String packageName = artefactName.substring(0, index).replace(pathSeparator, '.');
             if (packageName.matches("([a-zA-Z$_][a-zA-Z0-9$_]+)+(\\.[a-zA-Z$_][a-zA-Z0-9$_]+)*")) {
                 partialManifest.recordExportPackage(packageName.trim());
             }
